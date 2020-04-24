e16e541a5e492ed0dfd9e08c642a27d1bc4f9994
don undeen <donundeen@gmail.com>
2020-04-24 15:39:27 -0400

    changes

--- gbft/index.php
+++ gbft/index.php
@@ -26,6 +26,7 @@ unset($index->conn);
 $smarty->assign("index",$index);
 $smarty->assign("ERROR_MSGS", $ERROR_MSGS);
 
-$smarty->display("index.tpl");
+//$smarty->display("index.tpl");
+$smarty->display("./classes/SmartyTemplate/debug.tpl");
 
 ?>
