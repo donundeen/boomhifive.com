c2130433840ef40dbd6e6344af4594dc5067eda9
don undeen <donundeen@yahoo.com>
2025-04-13 18:38:20 -0400

    new notes

--- gbft/blocked_ip_check_t.php
+++ gbft/blocked_ip_check_t.php
@@ -11,8 +11,8 @@ $result = blocked_ip::ip_blocked($_SERVER['REMOTE_ADDR']);
 
 
 if($result){
-	echo "This ip Address, ".$_SERVER['REMOTE_ADDR'].", is blocked for being a jerk, for some reason. If you've got a problem with that, send an email to unblockme@undeen.com";
-	exit();
+	print "This ip Address, ".$_SERVER['REMOTE_ADDR'].", is blocked for being a jerk, for some reason. If you've got a problem with that, send an email to unblockme@undeen.com";
+//	exit();
 }else{
 }
 
