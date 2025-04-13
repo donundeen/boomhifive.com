f93048a4f83814748421873b567c18e7ac702158
don undeen <donundeen@yahoo.com>
2025-04-13 18:36:43 -0400

    new notes

--- gbft/setup_t.php
+++ gbft/setup_t.php
@@ -2,10 +2,10 @@
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 
-//include_once("blocked_ip_check.php");
-
-include_once('config.php');
+include_once("blocked_ip_check_t.php");
 /*
+include_once('config.php');
+
 $CITY_NAME = $GLOBALS['city_name'];
 $STATE_NAME = $GLOBALS['state_name'];
 $SHORT_NAME = $GLOBALS['short_name'];
