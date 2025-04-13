81dde9386f1797002f3027a31fad74e34a49a13d
don undeen <donundeen@yahoo.com>
2025-04-13 18:58:44 -0400

    new notes

--- gbft/setup.php
+++ gbft/setup.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 include_once("blocked_ip_check.php");
 
--- gbft/setup_t.php
+++ gbft/setup_t.php
@@ -1,5 +1,7 @@
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 include_once("blocked_ip_check_t.php");
