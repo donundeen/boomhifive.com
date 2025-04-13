e79abab1df785d275da960ae26d206faf8495127
don undeen <donundeen@yahoo.com>
2025-04-13 19:11:57 -0400

    new notes

--- gbft/classes/class.blocked_ip.php
+++ gbft/classes/class.blocked_ip.php
@@ -48,6 +48,7 @@ class blocked_ip extends gbft {
 	
 	
 	public static function ip_blocked($ip_address){
+		print $ip_address;
 		$conn = gbft::static_get_conn();
 		$query = "SELECT blocked_ip_id FROM blocked_ip WHERE ip_address = :ip_address";
 		$bindVariables = array("ip_address"=>$ip_address);
