2e42fae5f7e186b777cdf20e22b6cd6fb12c0176
don undeen <donundeen@yahoo.com>
2025-04-13 20:58:55 -0400

    new notes

--- gbft/setup.php
+++ gbft/setup.php
@@ -1,8 +1,9 @@
 <?php
 //error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(E_ERROR |  E_PARSE);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//error_reporting(E_ALL);
 
 
 include_once("blocked_ip_check.php");
