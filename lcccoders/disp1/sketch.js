acc76a9c5ec2814d4371813c5cf231f25ee031d8
Don Undeen <donundeen@gmail.com>
2025-09-17 10:32:32 -0400

    new notes

--- lcccoders/disp1/sketch.js
+++ lcccoders/disp1/sketch.js
@@ -1,219 +1,219 @@
-// initialize the variables that hold the images. We'll populate them in the preload function
-let skull, pears, ginger, guitar,eyeball, eyeball2, coke,pinkshag,catmelon, lemons, cereal;
+// click the > arrow thing just up and to the left from here.
+// then click the + sign next to "Sketch Files"
+// select "Upload file"
+// drag your images into the box that appears
+// load 10 images, one for each of the number 0-9 
+// something that looks like, or reminds you of, that number, 
+// that also represents something you like, are curious or passionate about
+// then change the image names below to match the name of the images you uploaded
 
-// initialize the variables that hold the sample bezier curve, that we use to generate values we can plug into the code.
-let bx1s, by1s, bc1xs, bc1ys, bc2xs, bc2ys, bx2s, by2s;
 
-// the object that holds the text of the bezier values
-let bezierText;
+// HINT: you can download images from the web, 
+// or use Command+Shift+4 to capture a selection of your screen and save it to the your desktop
+// or use a web app to take a picture w your laptop camera: 
+//     eg https://webcamtoy.com/#google_vignette or https://webcamtoy.com/ 
 
-// set this to true to show the sample bezier curve. Set it to false to hide it, when we want to just show the art
-let showSliders = true;
+// make sure to DUPLICATE the file before editing 
+//     (file->Duplicate and change the name to 00_Image_Clock_LastName_FirstName)
 
-// canvas dimensions
-let canvasWidth = 1200;
-let canvasHeight = 800;
+// and remember to SAVE (File->Save or Command+S)
 
-// Load the images and create a p5.Image objects.
+let clockTitle = "Mr. Undeen's Cool Clock"; // put a title for your clock here
+
+// change the image names below to match the names of the images you upload for each number
+let numberImages = [
+  "0Donut.jpg", // image that looks like a 0
+  "1Rocket.jpg", // image that looks like a 1
+  "2GuineaPigs.jpg", // image that looks like a 2
+  "3Cats.jpg", // image that looks like a 3
+  "4Pizza.jpeg", // image that looks like a 4
+  "5Saxes.png", // image that looks like a 5
+  "6Honeycomb.jpeg", // image that looks like a 6
+  "7TheSept.webp", // image that looks like a 7
+  "8Octopus.jpg", // image that looks like a 8
+  "9RubixCube.webp"  // image that looks like a 9
+];
+
+
+// these settings below you don't need to change, but you can, to see what happens.
+
+// the background color of the clock. Note all the different ways you can define colors.
+// try clicking on the colored square...
+let backgroundColor = "rgb(162,162,247)";
+
+// the width of each number 
+let imageWidth = 200;
+
+// coloring the dots in the colons
+// top dot in first colon
+let dot1fill = "green";
+let dot1StrokeWeight = 5;
+let dot1Stroke = "red";
+
+// bottom dot in first colon
+let dot2fill = "green";
+let dot2StrokeWeight = 5;
+let dot2Stroke = "red";
+
+// top dot in second colon
+let dot3fill = "rgb(241,255,0)";
+let dot3StrokeWeight = 5;
+let dot3Stroke = "red";
+
+// bottom dot in second colon
+let dot4fill = "rgb(241,255,0)";
+let dot4StrokeWeight = 5;
+let dot4Stroke = "red";
+
+// the width of the colon dot
+let dotWidth = 50;
+// the spacing between the dot and the numbers
+let dotMargin = 5;
+// the vertial spacing between the dots
+let dotVerticalSpacing = 150;
+
+// style information for the clock title
+let textFill = "yellow"; //inside of text
+let textStroke = "red"; // outline of text
+let textStrokeWeight = "3"; // how thick the outline is
+let textX = 20;  // the X position (starting from the left)
+let textY = 40;  // the Y position of the BOTTOM of the text, starting from the TOP of the screen
+let fontSize = 40;  // the text size
+
+// the height of the clock
+// (the width is calculated from the other elements)
+let canvasHeight = 400;
+
+
+
+
+
+
+
+// these are calculated values, you definitely don't want to change them.
+
+// calculate the width of the whole thing based on the imageWidth, the dotWidth, and the margins
+let canvasWidth = imageWidth * 6 + (dotWidth+dotMargin ) *2;
+
+// set the vertical positioning of the colon dots. Math!
+let upperDotY = (canvasHeight - dotVerticalSpacing + dotWidth) / 2;
+let lowerDotY = canvasHeight - upperDotY;
+
+let imgs = [];
+
+// Load the image and create a p5.Image object.
 function preload() {
-  skull   = loadImage('stillLifeWithSkullAndWritingQuill.jpeg');
-  pears   = loadImage('applesAndPears.jpeg');
-  ginger  = loadImage("ginger.jpeg");
-  guitar  = loadImage("guitar.jpeg");
-  eyeball = loadImage("eyeball.png");
-  eyeball2 = loadImage("eyeball2.jpg");
-  shag    = loadImage("pinkshag.jpeg");
-  coke    = loadImage("coke.png");
-  catmelon  = loadImage("catmelon.gif");
-  lemons    = loadImage("lemons.png");
-  cereal    = loadImage("cereal.png");
+  console.log("preload", numberImages);
+  for(let i = 0; i < numberImages.length; i++){
+    console.log("loading", numberImages[i]);
+    imgs[i] = loadImage(numberImages[i]);
+  }
 }
 
-// setup function: sets up the canvas and creates the slider elements for the sample bezier curve
+
 function setup() {
+  // this part runs once, and sets up the screen, aka the "CANVAS" that we're doing to draw onto
   createCanvas(canvasWidth, canvasHeight);
-  if(showSliders){
-    createSliders();
-  }
 }
 
 function draw() {
-  background(220);
+  // fill the whole backgroun with backgroundColor
+  background(backgroundColor);
   
-  // Draw the main background image
-  image(ginger, 0, 0, width, height, 0, 0, ginger.width, ginger.height, CONTAIN);
+  
+  // set up some variables that will hold each digit of the hour, minutes, and seconds
+  let h0, h1, m0, m1, s0, s1;
+  // these variables hold the images themselves
+  let h0img, h1img, m0img, m1img, s0img, s1img;
+  
+  // figure out all the digits of the time and assign them to those variables
+  let h = hour();  // this is by default 24 hour time. How could you change it to 12 hour time? (hint: use Modulo!)
+  let m = minute();
+  let s = second();
+  h0 = floor(h / 10); // divide a number by 10 and round DOWN with floor()
+  h1 = h % 10;
+  m0 = floor(m / 10);
+  m1 = m % 10;
+  s0 = floor(s / 10);
+  s1 = s % 10;
+  
+  console.log(h0,h1,m0,m1,s0,s1);
+  
+  h0img = imgs[h0];
+  h1img = imgs[h1];
+  m0img = imgs[m0];
+  m1img = imgs[m1];
+  s0img = imgs[s0];
+  s1img = imgs[s1];
+  
+  h0img.resize(200,0);
+  h1img.resize(200,0);
+  m0img.resize(200,0);
+  m1img.resize(200,0);
+  s0img.resize(200,0);
+  s1img.resize(200,0);
+  
+  let imageX = 0;
+  
+  
+  // show the hour images
+  image(h0img, imageX,   (canvasHeight - h0img.height)/2);
+  imageX += imageWidth; // add the width to get the next image X position
+  image(h1img, imageX, (canvasHeight - h1img.height)/2);
 
+  imageX += imageWidth + dotMargin; // add the width to get the next colon X position
   
-  push(); // save the old graphics state, create a new one for this part of the artwork
-  // call a function as a callback to clip() to make a clip mask we can put an image inside
-  clip(jarOpeningCircle); 
-  // put the  image in the clip mask, with positioning and size
-  image(cereal, 370, 150, cereal.width/4, cereal.height/4);
-  pop();
+  // show the colon dots
+  fill(dot2fill);
+  stroke(dot3Stroke);
+  strokeWeight(dot2StrokeWeight);
+  circle(imageX + dotWidth / 2, lowerDotY, dotWidth);
+  circle(imageX + dotWidth / 2, upperDotY, dotWidth);
   
-  push(); // save the old graphics state, create a new one for this part of the artwork
-  // call a function as a callback to clip() to make a clip mask we can put an image inside  
-  clip(bottleShape);
-  // put the  image in the clip mask, with positioning and size  
-  image(coke, 0, 10, width, height/2, 0,0, coke.width, coke.height, CONTAIN);
-  pop();
+  imageX += dotWidth + dotMargin; // add the width to get the next image X position
   
-
-  push(); // save the old graphics state, create a new one for this part of the artwork
-  // call a function as a callback to clip() to make a clip mask we can put an image inside    
-  clip(fabricRightShape);
-  // put the  image in the clip mask, with positioning and size    
-  image(shag, 0, 0, width, height, 0, 0, shag.width, shag.height, CONTAIN);
-  pop();
   
-  push(); // save the old graphics state, create a new one for this part of the artwork
-  // call a function as a callback to clip() to make a clip mask we can put an image inside      
-  clip(melonCircle);
-  // put the  image in the clip mask, with positioning and size      
-  image(catmelon, -170, 160, width, height/1.75, 0, 0, catmelon.width, catmelon.height, CONTAIN);  
-  pop();
+  // show the minute images
+  image(m0img, imageX, (canvasHeight - m0img.height)/2);
+  imageX += imageWidth;  // add the width to get the next image X position
+  image(m1img, imageX, (canvasHeight - m1img.height)/2);
   
-  push(); // save the old graphics state, create a new one for this part of the artwork
-  // call a function as a callback to clip() to make a clip mask we can put an image inside        
-  clip(lemonCircle);
-  // put the  image in the clip mask, with positioning and size      
-  image(lemons, 386,433, 180, 150);
-  pop();  
   
-  // show the bezier sliders, if showSliders is true
-  if(showSliders){
-    bezierTestDraw();
-  }
-  // show the mouse position
-  stroke("black");
-  fill("white");
-  text(mouseX + ", " + mouseY, 10, 10);
+  // show the colons
+  imageX += imageWidth + dotMargin ; // add the width to get the next colon X position
+  fill(dot3fill);
+  stroke(dot3Stroke);
+  strokeWeight(dot3StrokeWeight);
+  circle(imageX + dotWidth / 2, 150, dotWidth);
   
-}
-
-// create the shape that covers the lemon
-function lemonCircle(){
-  fill(200,0,0,80);
-  ellipse(453,484,60,60);
-  ellipse(428,494,20,20);
-}
-
-// create the shape that covers the melon
-function melonCircle(){
-  ellipse(411,390, 175, 175);
-}
-
-// create the shape that covers the jar opening
-function jarOpeningCircle(){
-  ellipse(456,179, 120, 40);
-}
-
-// create the shape that covers the bottle
-function bottleShape(){
-  beginShape();
-  fill(200,0,0,100);
-  vertex(580,90);
-  vertex(605,90);
-  vertex(607,100);
-  vertex(607,142);
-  vertex(627,176);
-  vertex(636,277);
-  vertex(603,268);
-  vertex(569,277);
-  vertex(563,188);
-  vertex(583,149);
+  fill(dot4fill);
+  stroke(dot4Stroke);
+  strokeWeight(dot4StrokeWeight);
+  circle(imageX + dotWidth / 2, 250, dotWidth);
   
-  endShape();
-  ellipse(595,91,40,20);
+  imageX += dotWidth + dotMargin; // add the width to get the next image X position
   
-}
-
-// create the shape that covers the fabric shape around the plants
-function fabricRightShape(){
-  fill(255,0,0,100);
-  beginShape();
-  //637, 467, 570, 514, 623, 547, 743, 506
-  vertex(637,467);
-  bezierVertex(570, 514, 623, 547, 743, 506);
-  vertex(717,563);
-  bezierVertex( 651, 531, 640, 552, 523, 583);
-  bezierVertex(522, 508, 619, 459, 609, 465);
-  endShape();
-}
-
-
-
-
-// functions for creating the UI sliders and showing the text Bezier curve
-function createSliders(){
+  // show the second images
+  image(s0img, imageX, (canvasHeight - s0img.height)/2);
+  imageX += imageWidth; // add the width to get the next image X position
+  image(s1img, imageX,(canvasHeight - s1img.height)/2);
   
-  bx1s = createSlider(0, canvasWidth);
-  bx1s.position(10, 10);
-  bx1s.size(200);
-  
-  by1s = createSlider(0, canvasHeight);
-  by1s.position(10, 25);
-  by1s.size(200);
-  
-  bc1xs = createSlider(0, canvasWidth);
-  bc1xs.position(10, 40);
-  bc1xs.size(200);
-  
-  bc1ys = createSlider(0, canvasHeight);
-  bc1ys.position(10, 55);
-  bc1ys.size(200);
-  
-  bc2xs = createSlider(0, canvasWidth);
-  bc2xs.position(10, 70);
-  bc2xs.size(200);
-  
-  bc2ys = createSlider(0, canvasHeight);
-  bc2ys.position(10, 85);
-  bc2ys.size(200);
+  fill(textFill);
+  stroke(textStroke);
+  strokeWeight(textStrokeWeight);
+  textSize(fontSize);
+  text(clockTitle, textX, textY);
     
-  bx2s = createSlider(0, canvasWidth);
-  bx2s.position(10, 100);
-  bx2s.size(200);
-  
-  by2s = createSlider(0, canvasHeight);
-  by2s.position(10, 115);
-  by2s.size(200);  
-  
-  bezierText = createInput("bezierText");
-  bezierText.style("color:black;");
-  bezierText.size(300);
-  bezierText.position(10, 130);
- // bezierText.html("bezierText");
-  
 }
 
 
-function bezierTestDraw(){
-  // make the control point dots, and lines from control points to start and end points
-  push();  // push set up a isolated graphics state
-  fill("white");
-  stroke("black");
-  circle( bc1xs.value(), bc1ys.value(),5);
-  circle( bc2xs.value(), bc2ys.value(),5);
-  stroke("white");
-  line(bc1xs.value(), bc1ys.value(), bx1s.value(), by1s.value());
-  line(bc1xs.value(), bc1ys.value(), bx2s.value(), by2s.value());
-  line(bc2xs.value(), bc2ys.value(), bx1s.value(), by1s.value());
-  line(bc2xs.value(), bc2ys.value(), bx2s.value(), by2s.value());
-  pop(); // restore old state
 
-  
-  // create the Bezier Curve
-  
-  push(); // store old graphics state
-  stroke(255,0,0,100);
-  fill(0,0,255,100);
-//  console.log(bx1s.value());
-  strokeWeight(2);
-  bezier(bx1s.value(), by1s.value(), bc1xs.value(), bc1ys.value(), bc2xs.value(), bc2ys.value(), bx2s.value(), by2s.value());
-  pop(); // restore old graphics state
-  
-  // fill out the bezier curve text so people can copy and paste it back into the code.
-  bezierText.value(bx1s.value() +", "+ by1s.value() +", "+  bc1xs.value() +", "+  bc1ys.value() +", "+  bc2xs.value() +", "+  bc2ys.value() +", "+  bx2s.value() +", "+  by2s.value()); 
-  
+
+// If the mouse is pressed,
+// toggle full-screen mode.
+function mousePressed() {
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
 }
